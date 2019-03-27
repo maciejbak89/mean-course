a44d2725d3a1e4740e5b3a0587c7d3c9a11df84f
Maciej Bak <mbak704@gmail.com>
2019-03-27 14:25:53 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+    private posts: Post[] = [];
+    private postsUpdated = new Subject<{posts: Post[], postCount: number}>();
+
+    constructor(private httpClient: HttpClient,
+                private router: Router) {}
+
+    getPosts(postsPerPage: number, currentPage: number) {
+        const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
+        this.httpClient
+            .get<{message: string, posts: any, maxPosts: number}>('http://localhost:3000/api/posts' + queryParams)
+            .pipe(map((postData) => {
+                return {
+                    posts: postData.posts.map((post) => {
+                        return {
+                            title: post.title,
+                            content: post.content,
+                            id: post._id,
+                            imagePath: post.imagePath
+                        };
+                    }),
+                    maxPosts: postData.maxPosts
+                };
+            }))
+            .subscribe((mappedPostData) => {
+                this.posts = mappedPostData.posts;
+                this.postsUpdated.next({
+                    posts: [...this.posts],
+                    postCount: mappedPostData.maxPosts
+                });
+            });
+    }
+
+    getPostUpdateListener() {
+        return this.postsUpdated.asObservable();
+    }
+
+    getPost(id: string) {
+        return this.httpClient.get<{_id: string, title: string, content: string, imagePath: string}>(
+            'http://localhost:3000/api/posts/' + id
+        );
+    }
+
+    addPost(title: string, content: string, image: File) {
+        const postData = new FormData();
+        postData.append('title', title);
+        postData.append('content', content);
+        postData.append('image', image, title);
+        this.httpClient
+            .post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData)
+            .subscribe((responseData) => {
+                this.router.navigate(['/']);
+            });
+    }
+
+    updatePost(id: string, title: string, content: string, image: File | string) {
+        let postData: Post | FormData;
+        if (typeof(image) === 'object') {
+            postData = new FormData();
+            postData.append('id', id);
+            postData.append('title', title);
+            postData.append('content', content);
+            postData.append('image', image, title);
+        } else {
+            postData = {
+                id: id,
+                title: title,
+                content: content,
+                imagePath: image
+            };
+        }
+        this.httpClient.put('http://localhost:3000/api/posts/' + id, postData)
+            .subscribe((response) => {
+                this.router.navigate(['/']);
+            });
+    }
+
+    deletePost(postId: string) {
+        return this.httpClient.delete('http://localhost:3000/api/posts/' + postId);
+    }
+}
