529a4c2d33d9681fe8b278322730647f5c046a70
Maciej Bak <mbak704@gmail.com>
2019-03-29 01:52:41 -0500

    Handling errors added

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -48,6 +48,11 @@ router.post(
                 id: createdPost._id
             }
         });
+    })
+    .catch((error) => {
+        res.status(500).json({
+            message: 'Creating a post failed!'
+        });
     });
 });
 
@@ -79,6 +84,11 @@ router.put(
                     message: 'Not authorized!'
                 });
             }
+        })
+        .catch((error) => {
+            res.status(500).json({
+                message: `Couldn't update post!`
+            });
         });
 });
 
@@ -103,6 +113,11 @@ router.get('', (req, res, next) => {
                 posts: fetchedPosts,
                 maxPosts: count
             });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                message: 'Fetching posts failed!'
+            });
         });
 });
 
@@ -114,6 +129,11 @@ router.get('/:id', (req, res, next) => {
             } else {
                 res.status(404).json({message: 'Post not found!'});
             }
+        })
+        .catch((error) => {
+            res.status(500).json({
+                message: 'Fetching post failed!'
+            });
         });
 });
 
@@ -129,6 +149,11 @@ router.delete('/:id', checkAuth, (req, res, next) => {
                     message: 'Not authorized!'
                 });
             }
+        })
+        .catch((error) => {
+            res.status(500).json({
+                message: 'Deleting post failed!'
+            });
         });
 });
 
