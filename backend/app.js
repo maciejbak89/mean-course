f706420af80e3796c784ecfbb4b8d7b533e91d68
Maciej Bak <mbak704@gmail.com>
2019-03-30 00:06:48 -0500

    App deployment - integrated approach - RESTful Api & Angular together

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,8 @@ mongoose.connect('mongodb+srv://maciejbak:' + process.env.MONGO_ATLAS_PW + '@clu
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/images', express.static(path.join('images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -29,5 +30,8 @@ app.use((req, res, next) => {
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+});
 
 module.exports = app;
